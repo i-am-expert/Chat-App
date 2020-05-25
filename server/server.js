f7d3540be9657b50d5911ce8da6f9f97e4d4efcd
i-am-expert <onlyforgithub2708@gmail.com>
2020-05-25 22:49:09 +0530

    Added connect and disconnect events

--- server/server.js
+++ server/server.js
@@ -1,12 +1,27 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '/../public');
 const port = process.env.PORT || 3000;
-const app = express();
+let app = express();
+let server = http.createServer(app)
+// We create our own server because we need to pass instance of server in below line
+let io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+// listen for 'connection' event
+io.on('connection', (socket) => {
+    // when someone hits to index.html
+    console.log('A new user just connected');
+    socket.on('disconnect', (socket) => {
+        // when user disconnects (closes tab)
+        console.log('User was disconnected');
+    })
+});
+
+server.listen(port, () => {
     console.log(`Server running on port ${port}`);
 })
